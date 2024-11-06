f14643cd62f412e17d86a05836c1a5c5980bbd68
Nicholas Rodrigues Lordello <n@lordello.net>
2024-11-06 19:57:25 +0100

    log host and port

--- src/server.py
+++ src/server.py
@@ -1,5 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+host = '0.0.0.0'
+port = 8999
+
 class CORSRequestHandler(SimpleHTTPRequestHandler):
   def end_headers(self):
     self.send_header('Access-Control-Allow-Origin', '*')
@@ -7,5 +10,6 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
     return super(CORSRequestHandler, self).end_headers()
 
-httpd = HTTPServer(('0.0.0.0', 8999), CORSRequestHandler)
+print(f'serving at http://{host}:{port}')
+httpd = HTTPServer((host, port), CORSRequestHandler)
 httpd.serve_forever()
